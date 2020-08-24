c5373d2f037adbdd5c5530dfdc955049fa31cd05
vilsonfcastilho <vilsonfcastilho@gmail.com>
2020-08-24 18:47:32 -0300

    layout signin and signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 
 import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
--- frontend/src/Pages/SignUp/styles.ts
+++ frontend/src/Pages/SignUp/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
-import signInBackgroundImg from '../../assets/sign-in-background.png';
+import signUpBackgroundImg from '../../assets/sign-up-background.png';
 
 export const Container = styled.div`
   height: 100vh;
@@ -44,7 +44,7 @@ export const Content = styled.div`
   > a {
     display: flex;
     align-items: center;
-    color: #ff9000;
+    color: #f4ede8;
     text-decoration: none;
     margin-top: 24px;
     transition: color 0.2s;
@@ -54,13 +54,13 @@ export const Content = styled.div`
     }
 
     &:hover {
-      color: ${shade(0.2, '#ff9000')};
+      color: ${shade(0.2, '#f4ede8')};
     }
   }
 `;
 
 export const Background = styled.div`
   flex: 1;
-  background: url(${signInBackgroundImg}) no-repeat center;
+  background: url(${signUpBackgroundImg}) no-repeat center;
   background-size: cover;
 `;
