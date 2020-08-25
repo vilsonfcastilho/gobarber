430eb48936a105f2a81feb3cbff46763622abc64
vilsonfcastilho <vilsonfcastilho@gmail.com>
2020-08-25 14:09:36 -0300

    finishing signin and signup layout

--- frontend/src/components/Input/styles.ts
+++ frontend/src/components/Input/styles.ts
@@ -1,19 +1,47 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+import Tooltip from '../Tooltip';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   display: flex;
   align-items: center;
 
   background: #232129;
-  border: 2px solid #232129;
   border-radius: 10px;
   padding: 16px;
-  color: #666360;
   width: 100%;
+
+  border: 2px solid #232129;
+  color: #666360;
 
   & + div {
     margin-top: 8px;
   }
+
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${props =>
+    props.isFocused &&
+    css`
+      border-color: #ff9000;
+      color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
 
   input {
     flex: 1;
@@ -28,5 +56,23 @@ export const Container = styled.div`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
